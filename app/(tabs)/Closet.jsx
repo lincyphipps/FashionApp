3ebe4a91a2ba1e8265b20cccc521f312e79a1593
arian <ariannamariety@ufl.edu>
2025-03-18 13:02:59 -0400

    closet screen

--- app/(tabs)/Closet.jsx
+++ app/(tabs)/Closet.jsx
@@ -1,25 +1,138 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, FlatList, Image } from 'react-native';
 
 export default function functionCloset() {
+
+    const [selectedTab, setSelectedTab] = useState(0);
+
+    const tabs = ['Recents', 'Tops', 'Bottoms', 'Accessories'];
+
+    const handleTabPress = (index) => {
+        setValue(index);
+    };
+
+    const imageData = {
+        Recents: [
+            require('../../assets/images/react-logo.png'),
+            require('../../assets/images/adaptive-icon.png'),
+            require('../../assets/images/react-logo.png'),
+            require('../../assets/images/react-logo.png'),
+        ],
+        Tops: [
+            require('../../assets/images/react-logo.png'),
+            require('../../assets/images/adaptive-icon.png'),
+            require('../../assets/images/react-logo.png'),
+        ],
+        Bottoms: [
+            require('../../assets/images/adaptive-icon.png'),
+            require('../../assets/images/react-logo.png'),
+        ],
+        Accessories: [
+            require('../../assets/images/adaptive-icon.png'),
+            require('../../assets/images/react-logo.png'),
+            require('../../assets/images/adaptive-icon.png'),
+            require('../../assets/images/react-logo.png'),
+        ],
+    };
+
+    const renderItem = ({item}) => (
+        <View style={styles.imageContainer}>
+            <Image source={item} style={styles.image} />
+        </View>
+    );
+
     return (
-        <View style={styles.titleContainer}>
-            <Text style={styles.title}>My Closet</Text>
+        <View style={styles.screen}>
+            <View style={styles.topTitleContainer}>
+                <Text style={styles.topTitleText}>
+                    My Closet
+                </Text>
+            </View>
+
+            <View style={styles.tabBar}>
+                {tabs.map((tab, index) => (
+                    <TouchableOpacity
+                        key={index}
+                        style={[styles.tabItem, selectedTab === index&&styles.activeTab]}
+                        onPress={() => setSelectedTab(index)}
+                    >
+                        <Text style={[styles.tabText, selectedTab === index && styles.activeTabText]}>{tab}</Text>
+                    </TouchableOpacity>
+                ))}
+            </View>
+            <FlatList
+                data={imageData[tabs[selectedTab]]}
+                renderItem={renderItem}
+                keyExtractor={(item, index) => index.toString()}
+                numColumns={2}
+                contentContainerStyle={styles.grid}
+            />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    titleContainer: {
+    screen: {
         flex: 1,
-        backgroundColor: '#E8DEF8',
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: '#fffcee',
     },
-    title: {
-        fontSize: 20,
+    topTitleText: {
+        fontSize: 40,
         fontWeight: 'bold',
+        backgroundColor: '#e8def8',
+        padding: 10,
+        paddingHorizontal: 30,
+        color: 'white',
+        textShadowColor: 'black',
+        textShadowOffset: {width: 2, height: 2},
+        textShadowRadius: 5,
+        borderColor: 'black',
+        borderRadius: 10,
+    },
+    topTitleContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: 60,
+    },
+    tabBar: {
+        flexDirection: 'row',
+        justiftContent: 'space-around',
+        paddingVertical: 10,
+    },
+    tabItem: {
+        paddingVertical: 8,
+        paddingInline: 15,
+        borderBottomWidth: 1,
+        borderBottomColor: 'gray',
+    },
+    tabText: {
+        fontSize: 20,
+        color: 'gray'
+    },
+    activeTab: {
+        borderBottomWidth: 2,
+        borderBottomColor: 'black',
+    },
+    activeTabText: {
+        color: 'black',
+        fontWeight: 'bold'
+    },
+    grid: {
+        padding: 10,
+        alignItems: 'center',
+    },
+    imageContainer: {
+        backgroundColor: 'black',
+        margin: 10,
+        borderRadius: 20,
+        width: 155,
+        height: 155,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    image: {
+        width: 155,
+        height: 155,
+        tintColor: 'white',
     },
 });
-
-//export default Closet; //this is the default export
